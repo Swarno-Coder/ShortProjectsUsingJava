72f4f28d38feb38d57f7ec191c388d91effe14c7
ANIRUDHA PRADHAN <114860976+Anirudha0702@users.noreply.github.com>
2023-10-02 18:29:11 +0530

    Update TemperatureConverter.java

--- src/TemperatureConverter.java
+++ src/TemperatureConverter.java
@@ -1,2 +1,31 @@
+import java.util.Scanner;
+
 public class TemperatureConverter {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Temperature Converter");
+        System.out.println("1. Fahrenheit to Celsius");
+        System.out.println("2. Celsius to Fahrenheit");
+        System.out.print("Enter your choice (1 or 2): ");
+        
+        int choice = scanner.nextInt();
+        
+        if (choice == 1) {
+            System.out.print("Enter temperature in Fahrenheit: ");
+            double fahrenheit = scanner.nextDouble();
+            double celsius = (fahrenheit - 32) * 5/9;
+            System.out.println("Temperature in Celsius: " + celsius);
+        } else if (choice == 2) {
+            System.out.print("Enter temperature in Celsius: ");
+            double celsius = scanner.nextDouble();
+            double fahrenheit = (celsius * 9/5) + 32;
+            System.out.println("Temperature in Fahrenheit: " + fahrenheit);
+        } 
+        else {
+            System.out.println("Invalid choice. Please enter 1 or 2.");
+        }
+        
+        scanner.close();
+    }
 }
