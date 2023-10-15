f4b5a17352a0219ed13b9f8f48bf599f1e24cb2b
SHIWANSHI TRIPATHI <112747153+TRIPATHISHIWANSHI@users.noreply.github.com>
2023-10-15 16:49:24 +0530

    Create ToDoList.java

---
+++ src/ToDoList.java
@@ -0,0 +1,42 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class ToDoList {
+    public static void main(String[] args) {
+        ArrayList<String> tasks = new ArrayList<>();
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("To-Do List Menu:");
+            System.out.println("1. Add Task");
+            System.out.println("2. View Tasks");
+            System.out.println("3. Exit");
+            System.out.print("Choose an option: ");
+            
+            int choice = scanner.nextInt();
+            scanner.nextLine(); // Consume the newline character
+
+            switch (choice) {
+                case 1:
+                    System.out.print("Enter a task: ");
+                    String task = scanner.nextLine();
+                    tasks.add(task);
+                    System.out.println("Task added!\n");
+                    break;
+                case 2:
+                    System.out.println("Tasks:");
+                    for (int i = 0; i < tasks.size(); i++) {
+                        System.out.println((i + 1) + ". " + tasks.get(i));
+                    }
+                    System.out.println();
+                    break;
+                case 3:
+                    System.out.println("Exiting the program.");
+                    scanner.close();
+                    System.exit(0);
+                default:
+                    System.out.println("Invalid choice. Please try again.\n");
+            }
+        }
+    }
+}
